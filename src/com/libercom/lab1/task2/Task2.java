e52be56a9be2f84de24ba8f1b11991d5a06d32de
Vasile <ignatvasea@gmail.com>
2021-09-27 21:23:46 +0300

    Created repository

---
+++ src/com/libercom/lab1/task2/Task2.java
@@ -0,0 +1,7 @@
+package com.libercom.lab1.task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/libercom/lab1/task3/Task3.java
@@ -0,0 +1,7 @@
+package com.libercom.lab1.task3;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+}
